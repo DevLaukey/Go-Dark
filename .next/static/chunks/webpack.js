b80da50678f98821efabad4bcc336757aa1a9d5b
DevLaukey <devlaukeymwaura@gmail.com>
2021-09-03 22:15:55 +0300

    Repsonsiveness check

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ec8005f695c783a51f7"; }
+/******/ 		__webpack_require__.h = function() { return "aab38fa9ccacf1cb13e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c814347d931a324e89c6.hot-update.js
+++ .next/static/webpack/webpack.c814347d931a324e89c6.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8da959e9c127dfe605f5"; }
+/******/ 	__webpack_require__.h = function() { return "fc3baddc68ff10389042"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
