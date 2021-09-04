1ba5399bafd7ab1998d592d04c14183c067924be
DevLaukey <devlaukeymwaura@gmail.com>
2021-09-04 16:10:06 +0300

    header changes made

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "317db644dbddf3bf67fe"; }
+/******/ 		__webpack_require__.h = function() { return "a8b2926383b5adf330d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
